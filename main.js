bac3baa8cd7fd85967d280f63dab9255c8225bf7
Michael Chan <michael@innova-global.com>
2018-03-05 04:24:24 +0900

    Build - used two conjugation dictionaries | next: continue

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-// Made some changes 3
+// Made some changes
 const Apify = require('apify');
 const typeCheck = require('type-check').typeCheck;
+const request = require('request-promise');
+const replace = require('lodash').replace;
+const uniq = require('lodash').uniq;
+
+const def_json = require('./dummy/def.json');
+// const conj_rv_json = require('./dummy/conj-rv.json');
+const conj_json = require('./dummy/conj-wr.json');
 
 // Definition of the input
 const INPUT_TYPE = `{
-    dict_def01_meaning: Maybe String,
-    dict_def01_eg01_mono: Maybe String,
+    query: String,
+    source: String,
+    dictionary: String
 }`;
+
+const LEVEL_TYPE = {
+    NOVOICE: 'NOVOICE',
+    INTERMEDIATE: 'INTERMEDIATE',
+    EXPERT: 'EXPERT'
+}
 
 Apify.main(async () => {
     // Fetch the input and check it has a valid format
@@ -20,14 +34,80 @@ Apify.main(async () => {
         throw new Error('Received invalid input');
     }
 
-    // Here's the place for your magic...
-    // console.log(`Input message: ${input.message}`);
-    console.log(`Input phrase: ${input.dict_def01_eg01_mono}`);
-
-    // Store the output
-    const output = {
-        // message: `${input.message} Hello my friend!`
-        phrase: `${input.dict_def01_eg01_mono}`
+    // https://github.com/request/request-promise
+    // Run act synchronously
+    const act_definition_api_option = {
+        method: 'POST',
+        uri: 'https://api.apify.com/v2/acts/igsys~linguee/run-sync?token=fDwYYKCbe2SzRfpTvMk4BGspj',
+        body: input,
+        json: true
     };
+    const act_conjugation_rv_api_option = {
+        method: 'POST',
+        uri: 'https://api.apify.com/v2/acts/igsys~conjugation-reverso/run-sync?token=fDwYYKCbe2SzRfpTvMk4BGspj',
+        body: input,
+        json: true
+    };
+    const act_conjugation_wr_api_option = {
+        method: 'POST',
+        uri: 'https://api.apify.com/v2/acts/igsys~conjugation-wordreference/run-sync?token=fDwYYKCbe2SzRfpTvMk4BGspj',
+        body: input,
+        json: true
+    };
+
+    const response = process.env.NODE_ENV === 'development'
+        ? [def_json, conj_json]
+        : await Promise.all([request(act_definition_api_option), request(act_conjugation_wr_api_option)])
+    const [def, conj] = response;
+    let phrases = [];
+
+    def.definitions.forEach(item => {
+        item.examples.forEach(example => {
+            let phrase = '';
+            let form_tenses = [];
+            conj.results.forEach(verb => {
+                if (example.mono.includes(verb.conjugation + ' ')) {
+                    console.log('example.mono.replace', replace(example.mono, verb.conjugation + ' ', `{{c1:${verb.conjugation}}} `))
+                    console.log('form:tense', verb.form, verb.tense)
+                    phrase = replace(example.mono, verb.conjugation + ' ', `{{c1:${verb.conjugation}}} `)
+                    form_tenses.push(`[${verb.form} - ${verb.tense}]`)
+                }
+            })
+            phrases.push({
+                keyword: input.query,
+                grammar: item.grammar,
+                meaning: item.meaning,
+                level: item.level,
+                // mono: replace(example.mono, verb.conjugation + ' ', `{{c1:${verb.conjugation}}} `),
+                mono: phrase,
+                tenses: uniq(form_tenses),
+                trans: example.trans
+            })
+        })
+    })
+
+    // def.definitions.forEach(item => {
+    //     item.examples.forEach(example => {
+    //         conj.conjugation.forEach(word => {
+    //             if (example.mono.includes(word)) {
+    //                 // console.log('example.mono:', example.mono)
+    //                 console.log('example.mono.replace', replace(example.mono, word, `{{c1:${word}}}`))
+    //                 phrases.push({
+    //                     keyword: input.query,
+    //                     grammar: item.grammar,
+    //                     meaning: item.meaning,
+    //                     level: item.level,
+    //                     mono: replace(example.mono, word, `{{c1:${word}}}`),
+    //                     trans: example.trans
+    //                 })
+    //             }
+    //         });
+    //     })
+    // })
+
+    const output = {
+        phrases
+    }
+    console.log('output', output)
     await Apify.setValue('OUTPUT', output)
 });
