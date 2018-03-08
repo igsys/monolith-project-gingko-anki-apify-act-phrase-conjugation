0a3357765a14337ecd394d08099eb28a9082759b
Michael Chan <michael@innova-global.com>
2018-03-08 15:40:17 +0900

    Build - changed gender ‘none' to 'unknown' | next: continue

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 // Made some changes
-const Apify = require('apify');
-const typeCheck = require('type-check').typeCheck;
-const request = require('request-promise');
-const replace = require('lodash').replace;
-const uniq = require('lodash').uniq;
+const Apify = require('apify')
+const typeCheck = require('type-check').typeCheck
+const request = require('request-promise')
+const replace = require('lodash').replace
+const uniq = require('lodash').uniq
 
-const def_json = require('./dummy/def.json');
-// const conj_rv_json = require('./dummy/conj-rv.json');
-const conj_json = require('./dummy/conj-wr.json');
+const def_json = require('./dummy/def.json')
+// const conj_rv_json = require('./dummy/conj-rv.json')
+const conj_json = require('./dummy/conj-wr.json')
 
 // Definition of the input
 const INPUT_TYPE = `{
-    id: Maybe String,
-    query: String,
-    source: String,
-    dictionary: String
-}`;
+    query: String
+    source: String
+    translation: String
+}`
 
 const LEVEL_TYPE = {
     NOVOICE: 'NOVOICE',
@@ -26,13 +25,13 @@ const LEVEL_TYPE = {
 Apify.main(async () => {
     // Fetch the input and check it has a valid format
     // You don't need to check the input, but it's a good practice.
-    const input = await Apify.getValue('INPUT');
+    const input = await Apify.getValue('INPUT')
     if (!typeCheck(INPUT_TYPE, input)) {
-        console.log('Expected input:');
-        console.log(INPUT_TYPE);
-        console.log('Received input:');
-        console.dir(input);
-        throw new Error('Received invalid input');
+        console.log('Expected input:')
+        console.log(INPUT_TYPE)
+        console.log('Received input:')
+        console.dir(input)
+        throw new Error('Received invalid input')
     }
 
     console.log('input', input)
@@ -44,34 +43,34 @@ Apify.main(async () => {
         uri: 'https://api.apify.com/v2/acts/igsys~linguee/run-sync?token=fDwYYKCbe2SzRfpTvMk4BGspj',
         body: input,
         json: true
-    };
+    }
     // const act_conjugation_rv_api_option = {
     //     method: 'POST',
     //     uri: 'https://api.apify.com/v2/acts/igsys~conjugation-reverso/run-sync?token=fDwYYKCbe2SzRfpTvMk4BGspj',
     //     body: input,
     //     json: true
-    // };
+    // }
     const act_conjugation_wr_api_option = {
         method: 'POST',
         uri: 'https://api.apify.com/v2/acts/igsys~conjugation-wordreference/run-sync?token=fDwYYKCbe2SzRfpTvMk4BGspj',
         body: input,
         json: true
-    };
+    }
 
     // wait until all branches of API request are done, and return results
     const response = process.env.NODE_ENV === 'development'
         ? [def_json, conj_json]
         : await Promise.all([request(act_definition_api_option), request(act_conjugation_wr_api_option)])
-    const [def, conj] = response;
+    const [def, conj] = response
 
     // replace conjugations
-    let phrases = [];
+    let phrases = []
     def.definitions.forEach(item => {
         item.examples.forEach(example => {
             // initialization
             let phrase = ''
             let form_tenses = []
-            let gender = 'm'
+            let gender = 'unknown'
 
             // replace conjugations to anki card syntax
             conj.results.forEach(verb => {
@@ -87,11 +86,11 @@ Apify.main(async () => {
             })
 
             // form tense string from unique array
-            form_tenses = uniq(form_tenses);
-            let tense = '';
+            form_tenses = uniq(form_tenses)
+            let tense = ''
             form_tenses.forEach(item => {
-                tense = tense + ' ' + item;
-            });
+                tense = tense + ' ' + item
+            })
 
             // push into phrases array for output
             phrases.push({
@@ -123,4 +122,4 @@ Apify.main(async () => {
     }
     console.log('output', output)
     await Apify.setValue('OUTPUT', output)
-});
+})
